e2c90daf0c149cafcffc72f88e40f8e9d877811e
PVXuan <phamvietxuan2000@gmail.com>
2021-11-25 21:55:45 +0700

    commit lai:)

--- Jupyter_nnmodel/feature_generater.py
+++ Jupyter_nnmodel/feature_generater.py
@@ -3,24 +3,28 @@ from itertools import combinations
 import numpy as np
 import pandas as pd
 
+'''
+- 
+'''
+
+
 def Multiply_Divide(train, test, features):
     """
     combinations:
     combinations(['A', 'B','C'],2)  retrun AB AC BC
     combinations(range(4), 3) --> 012 013 023 123
     """
-    feature_names= []
+    feature_names = []
+    # Tạo tổ hợp chập 2 các feature và đánh số cho từng tổ hợp bằng hàm emurates
     for e, (x, y) in enumerate(combinations(features, 2)):
-        train, test, feature_name= interaction_features(train, test, x, y, e)
+        # Thực hiện tạo thêm các cột mới là kết quả nhân/chia các feature x, y tương ứng
+        train, test, feature_name = interaction_features(train, test, x, y, e)
         for name in feature_name:
             feature_names.append(name)
 
-
     return train, test, feature_names
 
-
-
-
+#Tạo ra các series số 
 def Series_string(train, test, category_list):
     '''
     produce series as a string like new_ind as the following
@@ -35,8 +39,7 @@ def Series_string(train, test, category_list):
         feature_names = list(train.columns)
 
         features = [c for c in feature_names if category in c]
-        name= 'new_'+ category
-
+        name = 'new_' + category
 
         count = 0
         for c in features:
@@ -57,34 +60,35 @@ def Series_string(train, test, category_list):
     return train, test
 
 
-
 def Features_Counts(train, test, features):
-    feature_names =[]
+    feature_names = []
 
     for c in features:
-        d = pd.concat([train[c],test[c]]).value_counts().to_dict()
-        train['%s_count'%c] = train[c].apply(lambda x:d.get(x,0))
-        test['%s_count'%c] = test[c].apply(lambda x:d.get(x,0))
-        feature_names.append('%s_count'%c)
+        d = pd.concat([train[c], test[c]]).value_counts().to_dict()
+        train['%s_count' % c] = train[c].apply(lambda x: d.get(x, 0))
+        test['%s_count' % c] = test[c].apply(lambda x: d.get(x, 0))
+        feature_names.append('%s_count' % c)
 
     return train, test, feature_names
 
 
 def Statistic_features(train, test, target_features, group_features):
-    train_list_=[]
-    test_list_=[]
+    train_list_ = []
+    test_list_ = []
     for t in target_features:
         for g in group_features:
             if t != g:
-                s_train, s_test = proj_num_on_cat(train, test, target_column=t, group_column=g)
+                s_train, s_test = proj_num_on_cat(
+                    train, test, target_column=t, group_column=g)
                 train_list_.append(s_train)
                 test_list_.append(s_test)
     return np.hstack(train_list_), np.hstack(test_list_)
 
+
 def features_type(train):
     data = []
     for f in train.columns:
-        
+
         # Defining the level
         if 'bin' in f or f == 'target':
             level = 'binary'
@@ -94,15 +98,15 @@ def features_type(train):
             level = 'interval'
         elif train[f].dtype == int:
             level = 'ordinal'
-            
+
         # Initialize keep to True for all variables except for id
         keep = True
         if f == 'id':
             keep = False
-        
-        # Defining the data type 
+
+        # Defining the data type
         dtype = train[f].dtype
-        
+
         # Creating a Dict that contains all the metadata for the variable
         f_dict = {
             'varname': f,
@@ -116,9 +120,5 @@ def features_type(train):
     interval = meta[(meta.level == 'interval') & (meta.keep)].index
     ordinal = meta[(meta.level == 'ordinal') & (meta.keep)].index
     binary = meta[(meta.level == 'binary') & (meta.keep)].index
-    nominal  = meta[(meta.level == 'nominal') & (meta.keep)].index
+    nominal = meta[(meta.level == 'nominal') & (meta.keep)].index
     return interval, ordinal, binary, nominal
-
-
-
-
--- Jupyter_nnmodel/util.py
+++ Jupyter_nnmodel/util.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def Gini(y_true, y_pred):
     # check and get number of samples
     assert y_true.shape == y_pred.shape
@@ -30,7 +31,8 @@ def cat_count(train_df, test_df, cat_list):
     test_df['row_id'] = range(test_df.shape[0])
     train_df['train'] = 1
     test_df['train'] = 0
-    all_df = train_df[['row_id', 'train'] + cat_list].append(test_df[['row_id','train'] + cat_list])
+    all_df = train_df[['row_id', 'train'] +
+                      cat_list].append(test_df[['row_id', 'train'] + cat_list])
     for e, cat in enumerate(cat_list):
         grouped = all_df[[cat]].groupby(cat)
         the_size = pd.DataFrame(grouped.size()).reset_index()
@@ -41,10 +43,13 @@ def cat_count(train_df, test_df, cat_list):
         selected_test = all_df[all_df['train'] == 0]
         selected_train.sort_values('row_id', inplace=True)
         selected_test.sort_values('row_id', inplace=True)
-        selected_train.drop(['row_id', 'train'] + cat_list, axis=1, inplace=True)
-        selected_test.drop(['row_id', 'train'] + cat_list, axis=1, inplace=True)
+        selected_train.drop(['row_id', 'train'] +
+                            cat_list, axis=1, inplace=True)
+        selected_test.drop(['row_id', 'train'] +
+                           cat_list, axis=1, inplace=True)
 
-        selected_train, selected_test = np.array(selected_train), np.array(selected_test)
+        selected_train, selected_test = np.array(
+            selected_train), np.array(selected_test)
     print(selected_train.shape, selected_test.shape)
     return selected_train, selected_test
 
@@ -54,38 +59,38 @@ def proj_num_on_cat(train_df, test_df, target_column, group_column):
     :param train_df: train data frame
     :param test_df:  test data frame
     :param target_column: name of numerical feature
-    :param group_column: name of categorical feature
+    :param group_column: name of categorical feature m
     """
-    train_df['row_id'] = range(train_df.shape[0])  # 595211 create index for each row
-    
+    train_df['row_id'] = range(
+        train_df.shape[0])  # 595211 create index for each row
+
     test_df['row_id'] = range(test_df.shape[0])
     train_df['train'] = 1
     test_df['train'] = 0
-    
-    all_df = train_df[['row_id', 'train', target_column, group_column]].append(test_df[['row_id','train',
-                                                                                        target_column, group_column]]).copy()
-    
-    
-    #count the number
-    grouped = all_df[[target_column, group_column]].groupby(group_column)
-    
-    
-    #count the number of distint value  from the list [1,1, 2,3]   
-    #[1,2,3]  so answer is  3 
-    
-    #count the number of each distint value  [1,1,2,3]  
-    #1:2 
-    #2:1 
-    #3:1
-    
-    
-    #count the number of each distint value
-    the_size = pd.DataFrame(grouped.size()).reset_index()
-    the_size.columns = [group_column, '%s_size' % target_column]  #rename columns name
 
-    #find the mean, std, median, max, min of each distint value
+    all_df = train_df[['row_id', 'train', target_column, group_column]].append(test_df[['row_id', 'train',
+                                                                                        target_column, group_column]]).copy()
+
+    # count the number
+    grouped = all_df[[target_column, group_column]].groupby(group_column)
+
+    # count the number of distint value  from the list [1,1, 2,3]
+    # [1,2,3]  so answer is  3
+
+    # count the number of each distint value  [1,1,2,3]
+    # 1:2
+    # 2:1
+    # 3:1
+
+    # count the number of each distint value
+    the_size = pd.DataFrame(grouped.size()).reset_index()
+    the_size.columns = [group_column, '%s_size' %
+                        target_column]  # rename columns name
+
+    # find the mean, std, median, max, min of each distint value
     the_mean = pd.DataFrame(grouped.mean()).reset_index()
-    the_mean.columns = [group_column, '%s_mean' % target_column] #rename columns name
+    the_mean.columns = [group_column, '%s_mean' %
+                        target_column]  # rename columns name
     the_std = pd.DataFrame(grouped.std()).reset_index().fillna(0)
     the_std.columns = [group_column, '%s_std' % target_column]
     the_median = pd.DataFrame(grouped.median()).reset_index()
@@ -94,29 +99,32 @@ def proj_num_on_cat(train_df, test_df, target_column, group_column):
     the_max.columns = [group_column, '%s_max' % target_column]
     the_min = pd.DataFrame(grouped.min()).reset_index()
     the_min.columns = [group_column, '%s_min' % target_column]
-    
-    #merge them 
-    the_stats=pd.concat([the_size,the_mean.iloc[:,1],the_std.iloc[:,1]
-                         ,the_median.iloc[:,1] ,the_max.iloc[:,1],the_min.iloc[:,1]]
-                        ,axis=1, join_axes=[the_size.index])
 
-    #insert value to the original data
+    # merge them
+    the_stats = pd.concat([the_size, the_mean.iloc[:, 1], the_std.iloc[:, 1], the_median.iloc[:, 1],
+                          the_max.iloc[:, 1], the_min.iloc[:, 1]], axis=1, join_axes=[the_size.index])
+
+    # insert value to the original data
     all_df = pd.merge(all_df, the_stats, how='left')
 
-    #splite to train and test
+    # splite to train and test
     selected_train = all_df[all_df['train'] == 1].copy()
     selected_test = all_df[all_df['train'] == 0].copy()
     selected_train.sort_values('row_id', inplace=True)
     selected_test.sort_values('row_id', inplace=True)
-    
-    #remove target_column, group_column, 'row_id', 'train' columns
-    selected_train.drop([target_column, group_column, 'row_id', 'train'], axis=1, inplace=True)
-    selected_test.drop([target_column, group_column, 'row_id', 'train'], axis=1, inplace=True)
 
-    selected_train, selected_test = np.array(selected_train), np.array(selected_test)
+    # remove target_column, group_column, 'row_id', 'train' columns
+    selected_train.drop([target_column, group_column,
+                        'row_id', 'train'], axis=1, inplace=True)
+    selected_test.drop([target_column, group_column,
+                       'row_id', 'train'], axis=1, inplace=True)
+
+    selected_train, selected_test = np.array(
+        selected_train), np.array(selected_test)
     return selected_train, selected_test
 
 
+# Hàm này tạo feature mới có đầu là inter là những đặc trưng được nhân, chia từng cặp feature
 def interaction_features(train, test, fea1, fea2, prefix):
     train['inter_{}*'.format(prefix)] = train[fea1] * train[fea2]
     train['inter_{}/'.format(prefix)] = train[fea1] / train[fea2]
