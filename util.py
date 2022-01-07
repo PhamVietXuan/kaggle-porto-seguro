7f10524896eac655ff2ef37d469347981bf82339
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2022-01-07 17:01:52 +0700

    clean repo

--- util.py
+++ util.py
@@ -56,64 +56,44 @@ def proj_num_on_cat(train_df, test_df, target_column, group_column):
     :param target_column: name of numerical feature
     :param group_column: name of categorical feature
     """
-    train_df['row_id'] = range(train_df.shape[0])  # 595211 create index for each row
-    
+    train_df['row_id'] = range(train_df.shape[0])
     test_df['row_id'] = range(test_df.shape[0])
     train_df['train'] = 1
     test_df['train'] = 0
-    
     all_df = train_df[['row_id', 'train', target_column, group_column]].append(test_df[['row_id','train',
-                                                                                        target_column, group_column]]).copy()
-    
-    
-    #count the number
+                                                                                        target_column, group_column]])
     grouped = all_df[[target_column, group_column]].groupby(group_column)
-    
-    
-    #count the number of distint value  from the list [1,1, 2,3]   
-    #[1,2,3]  so answer is  3 
-    
-    #count the number of each distint value  [1,1,2,3]  
-    #1:2 
-    #2:1 
-    #3:1
-    
-    
-    #count the number of each distint value
     the_size = pd.DataFrame(grouped.size()).reset_index()
-    the_size.columns = [group_column, '%s_size' % target_column]  #rename columns name
-
-    #find the mean, std, median, max, min of each distint value
+    the_size.columns = [group_column, '%s_size' % target_column]
     the_mean = pd.DataFrame(grouped.mean()).reset_index()
-    the_mean.columns = [group_column, '%s_mean' % target_column] #rename columns name
+    the_mean.columns = [group_column, '%s_mean' % target_column]
     the_std = pd.DataFrame(grouped.std()).reset_index().fillna(0)
     the_std.columns = [group_column, '%s_std' % target_column]
     the_median = pd.DataFrame(grouped.median()).reset_index()
     the_median.columns = [group_column, '%s_median' % target_column]
+    the_stats = pd.merge(the_size, the_mean)
+    the_stats = pd.merge(the_stats, the_std)
+    the_stats = pd.merge(the_stats, the_median)
+
     the_max = pd.DataFrame(grouped.max()).reset_index()
     the_max.columns = [group_column, '%s_max' % target_column]
     the_min = pd.DataFrame(grouped.min()).reset_index()
     the_min.columns = [group_column, '%s_min' % target_column]
-    
-    #merge them 
-    the_stats=pd.concat([the_size,the_mean.iloc[:,1],the_std.iloc[:,1]
-                         ,the_median.iloc[:,1] ,the_max.iloc[:,1],the_min.iloc[:,1]]
-                        ,axis=1).reindex(the_size.index)
 
-    #insert value to the original data
+    the_stats = pd.merge(the_stats, the_max)
+    the_stats = pd.merge(the_stats, the_min)
+
     all_df = pd.merge(all_df, the_stats, how='left')
 
-    #splite to train and test
-    selected_train = all_df[all_df['train'] == 1].copy()
-    selected_test = all_df[all_df['train'] == 0].copy()
+    selected_train = all_df[all_df['train'] == 1]
+    selected_test = all_df[all_df['train'] == 0]
     selected_train.sort_values('row_id', inplace=True)
     selected_test.sort_values('row_id', inplace=True)
-    
-    #remove target_column, group_column, 'row_id', 'train' columns
     selected_train.drop([target_column, group_column, 'row_id', 'train'], axis=1, inplace=True)
     selected_test.drop([target_column, group_column, 'row_id', 'train'], axis=1, inplace=True)
 
     selected_train, selected_test = np.array(selected_train), np.array(selected_test)
+    print(selected_train.shape, selected_test.shape)
     return selected_train, selected_test
 
 
@@ -123,6 +103,5 @@ def interaction_features(train, test, fea1, fea2, prefix):
 
     test['inter_{}*'.format(prefix)] = test[fea1] * test[fea2]
     test['inter_{}/'.format(prefix)] = test[fea1] / test[fea2]
-    feature_name = ['inter_{}*'.format(prefix), 'inter_{}/'.format(prefix), ]
 
-    return train, test, feature_name
+    return train, test
